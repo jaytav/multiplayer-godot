e8e37b6b954b97bc9a80b1bf3f74b16ea6f44c43
Jayden Tavares <jayden.tavares@hotmail.com>
2024-01-05 10:55:43 +1100

    add functionality for HostController to create server and JoinController to create client

--- src/autoload/HostController.cs
+++ src/autoload/HostController.cs
@@ -2,8 +2,19 @@ using Godot;
 
 public partial class HostController : Node
 {
+    private ENetMultiplayerPeer _peer = new();
+
     public void OnMainMenuScreenHostButtonPressed()
     {
         GD.Print("HostController: HandleMainMenuHostButtonPressed()");
+
+        _peer.CreateServer(3000);
+        Multiplayer.MultiplayerPeer = _peer;
+        Multiplayer.PeerConnected += OnMultiplayerPeerConnected;
+    }
+
+    private void OnMultiplayerPeerConnected(long id)
+    {
+        GD.Print($"HostController: OnMultiplayerPeerConnected(): Connected with id: {id}");
     }
 }
--- src/autoload/JoinController.cs
+++ src/autoload/JoinController.cs
@@ -2,8 +2,19 @@ using Godot;
 
 public partial class JoinController : Node
 {
+    private ENetMultiplayerPeer _peer = new();
+
     public void OnMainMenuScreenJoinButtonPressed()
     {
         GD.Print("JoinController: HandleMainMenuJoinButtonPressed()");
+
+        _peer.CreateClient("127.0.0.1", 3000);
+        Multiplayer.MultiplayerPeer = _peer;
+        Multiplayer.PeerConnected += OnMultiplayerPeerConnected;
+    }
+
+    private void OnMultiplayerPeerConnected(long id)
+    {
+        GD.Print($"JoinController: OnMultiplayerPeerConnected(): Connected with id: {id}");
     }
 }
